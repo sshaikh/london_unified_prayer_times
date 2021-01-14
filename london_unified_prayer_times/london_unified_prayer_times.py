e2470644013d50a4ffe3e5cdb28e94eb1a282dec
sshaikh <sshaikh@users.noreply.github.com>
2021-01-14 15:09:52 +0000

    now validate

--- london_unified_prayer_times/london_unified_prayer_times.py
+++ london_unified_prayer_times/london_unified_prayer_times.py
@@ -2,9 +2,43 @@
 
 import urllib.request
 import json
+import jsonschema
+from jsonschema import validate
+
+
+lupt_schema = {
+    "$id": "https://github.com/sshaikh/london_unified_prayer_times",
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "type": "object",
+    "properties": {
+        "data": {
+            "type": "array",
+            "items": {"$ref": "#/definitions/day"}
+        }
+    },
+    "definitions": {
+        "day": {
+            "type": "object",
+            "required": ["gregoriandate"],
+            "properties": {
+                "gregoriandate": {
+                    "type": "string"
+                }
+            }
+        }
+    }
+}
 
 
 def get_json_data(url):
     with urllib.request.urlopen(url) as data:
         json_data = json.loads(data.read().decode())
         return json_data
+
+
+def validate_json(json):
+    try:
+        validate(json, lupt_schema)
+    except jsonschema.exceptions.ValidationError:
+        return False
+    return True
--- tests/test_london_unified_prayer_times.py
+++ tests/test_london_unified_prayer_times.py
@@ -3,6 +3,7 @@
 """Tests for `london_unified_prayer_times` package."""
 
 import pytest
+import json
 
 from click.testing import CliRunner
 
@@ -21,8 +22,72 @@ def test_command_line_interface():
     assert '--help  Show this message and exit.' in help_result.output
 
 
+@pytest.fixture
+def single_day():
+    return json.loads("""
+    {
+        "data": [
+            {
+                "gregoriandate": "2020-09-30T23:00:00.000Z",
+                "islamicday": "14",
+                "islamicmonth": "Safar",
+                "islamicyear": "1442",
+                "sunrise": "6:59",
+                "fajrbegins": "5:31",
+                "fajrjamāah": "6:01",
+                "zuhrbegins": "12:55",
+                "zuhrjamāah": "1:30",
+                "asrmithl1": "3:55",
+                "asrmithl2": "4:43",
+                "asrjamāah": "5:00",
+                "maghribbegins": "6:40",
+                "maghribjamāah": "6:47",
+                "ishābegins": "7:58",
+                "ishājamāah": "8:15"
+            }
+        ]
+    }
+""")
+
+
+@pytest.fixture
+def bad_json():
+    return json.loads("""
+    {
+        "data": [
+            {
+                "gregosiandate": "2020-09-30T23:00:00.000Z",
+                "islamicday": "14",
+                "islamicmonth": "Safar",
+                "islamicyear": "1442",
+                "sunrise": "6:59",
+                "fajrbegins": "5:31",
+                "fajrjamāah": "6:01",
+                "zuhrbegins": "12:55",
+                "zuhrjamāah": "1:30",
+                "asrmithl1": "3:55",
+                "asrmithl2": "4:43",
+                "asrjamāah": "5:00",
+                "maghribbegins": "6:40",
+                "maghribjamāah": "6:47",
+                "ishābegins": "7:58",
+                "ishājamāah": "8:15"
+            }
+        ]
+    }
+""")
+
+
 @pytest.mark.vcr()
 def test_get_json_data():
     url = ("https://mock.location.com/lupt")
     json = lupt.get_json_data(url)
     assert json is not None
+
+
+def test_validate_json(single_day):
+    assert lupt.validate_json(single_day)
+
+
+def test_invalid_json(bad_json):
+    assert not lupt.validate_json(bad_json)
