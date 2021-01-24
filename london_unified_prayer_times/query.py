cc483eb7fc0b3b65bc4ef65c7c6c3ada4f3b8603
sshaikh <sshaikh@users.noreply.github.com>
2021-01-24 16:56:43 +0000

    can now query timetables

---
+++ london_unified_prayer_times/query.py
@@ -0,0 +1,30 @@
+from london_unified_prayer_times import constants
+
+
+tk = constants.TimetableKeys
+
+
+def get_islamic_date(timetable, date, when):
+    return timetable[tk.DATES][date][tk.ISLAMIC_DATES][when]
+
+
+def get_islamic_date_today(timetable, date):
+    return get_islamic_date(timetable, date, tk.TODAY)
+
+
+def get_islamic_date_tomorrow(timetable, date):
+    return get_islamic_date(timetable, date, tk.TOMORROW)
+
+
+def get_available_times(timetable):
+    dates = timetable[tk.DATES]
+
+    if len(dates) == 0:
+        return []
+
+    day = next(iter(dates.values()))
+    return day[tk.TIMES].keys()
+
+
+def get_time(timetable, date, time):
+    return timetable[tk.DATES][date][tk.TIMES][time]
---
+++ tests/test_query.py
@@ -0,0 +1,40 @@
+import datetime
+from london_unified_prayer_times import query
+from london_unified_prayer_times import constants
+from london_unified_prayer_times import timetable
+from . import test_timetable
+
+
+tk = constants.TimetableKeys
+
+
+def test_query_islamic_date(three_day_timetable):
+    today = query.get_islamic_date_today(three_day_timetable,
+                                         datetime.date(2020, 10, 2))
+    assert today == (1442, "Safar", 15)
+
+
+def test_query_islamic_date_tomorrow(three_day_timetable):
+    tomorrow = query.get_islamic_date_tomorrow(three_day_timetable,
+                                               datetime.date(2020, 10, 2))
+    assert tomorrow == (1442, "Safar", 16)
+
+
+def test_query_available_times(three_day_timetable):
+    list_times = query.get_available_times(three_day_timetable)
+
+    assert len(list_times) == 12
+
+
+def test_query_available_times_is_zero():
+    tt = timetable.create_empty_timetable()
+    list_times = query.get_available_times(tt)
+
+    assert len(list_times) == 0
+
+
+def test_query_time(three_day_timetable):
+    time = query.get_time(three_day_timetable,
+                          datetime.date(2020, 10, 2),
+                          "sunrise")
+    assert time == test_timetable.create_utc_datetime(2020, 10, 2, 6, 0)
