1be576517afa13a3925c855e00baca49512f18b5
sshaikh <sshaikh@users.noreply.github.com>
2021-01-27 20:37:50 +0000

    cli test for database refresh

--- london_unified_prayer_times/cli.py
+++ london_unified_prayer_times/cli.py
@@ -2,14 +2,32 @@
 import sys
 import click
 
+from . import cache
+from . import config
+from . import constants
 
-@click.command()
+
+@click.group()
 def main(args=None):
     """Console script for london_unified_prayer_times."""
-    click.echo("Replace this message by putting your code into "
-               "london_unified_prayer_times.cli.main")
-    click.echo("See click documentation at https://click.palletsprojects.com/")
     return 0
+
+
+@main.command()
+# @main.option('--url', '-u', help='URL to retrieve times from')
+def refresh():
+    url = 'plop'
+    schema = config.lupt_schema
+    pickle_filename = constants.PICKLE_FILENAME
+    tt = cache.refresh_timetable(url, schema, config, pickle_filename)
+    if tt:
+        num_dates = len(tt[constants.TimetableKeys.DATES])
+        click.echo(f'Successfully refreshed {num_dates}' +
+                   ' dates into ' +
+                   pickle_filename)
+        return 0
+
+    return -1
 
 
 if __name__ == "__main__":
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,7 +8,18 @@ def test_command_line_interface():
     runner = CliRunner()
     result = runner.invoke(cli.main)
     assert result.exit_code == 0
-    assert 'london_unified_prayer_times.cli.main' in result.output
+    assert '--help  Show this message and exit.' in result.output
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
     assert '--help  Show this message and exit.' in help_result.output
+
+
+def test_refresh_database(three_day_timetable, mocker):
+
+    mocker.patch('london_unified_prayer_times.cli.cache.refresh_timetable',
+                 return_value=three_day_timetable)
+
+    runner = CliRunner()
+    result = runner.invoke(cli.main, ['refresh'])
+    assert result.exit_code == 0
+    assert 'Successfully refreshed 3 dates into timetable' in result.output
